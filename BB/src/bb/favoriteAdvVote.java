d79861cf11b98622537ec9346cbc037fa2f1d384
Joseph Mahiya <jm0117@mix.wvu.edu>
2019-04-20 15:58:30 -0400

    various changes III

--- BB/src/bb/favoriteAdvVote.java
+++ BB/src/bb/favoriteAdvVote.java
@@ -57,6 +57,11 @@ public class favoriteAdvVote extends javax.swing.JFrame {
         });
 
         jButton2.setText("Back");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
 
         jButton3.setText("Cancel");
 
@@ -117,6 +122,11 @@ public class favoriteAdvVote extends javax.swing.JFrame {
         
         
     }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        // TODO add your handling code here:
+        this.setVisible(false);
+    }//GEN-LAST:event_jButton2ActionPerformed
 
     public String getSelectedAdvisor(){
        // selected = jList1.getSelectedValue().toString();
