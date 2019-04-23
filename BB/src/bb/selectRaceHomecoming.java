de4fb462d629035cb73804f8fec43de7bed8fe33
Joseph Mahiya <jm0117@mix.wvu.edu>
2019-04-23 14:55:36 -0400

    back and next functionality

--- BB/src/bb/selectRaceHomecoming.java
+++ BB/src/bb/selectRaceHomecoming.java
@@ -118,11 +118,13 @@ public class selectRaceHomecoming extends javax.swing.JFrame {
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         // TODO add your handling code here:
+        this.setVisible(false);
         new queen().setVisible(true);
     }//GEN-LAST:event_jButton1ActionPerformed
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
         // TODO add your handling code here:
+        this.setVisible(false);
         new king().setVisible(true);
     }//GEN-LAST:event_jButton2ActionPerformed
 
